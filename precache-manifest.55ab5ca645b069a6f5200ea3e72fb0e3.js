eec1c2cdf0652d38f76169bef3104cbcea4c9ef9
Harshit Anand <harshitanand22@gmail.com>
2023-01-08 20:05:55 +0530

    Updates

---
+++ precache-manifest.55ab5ca645b069a6f5200ea3e72fb0e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a4ab6f8047a566d7071",
+    "url": "origin%20https://Harshit2299.github.io/TextUtils-React/static/css/main.2399a1f4.chunk.css"
+  },
+  {
+    "revision": "5a4ab6f8047a566d7071",
+    "url": "origin%20https://Harshit2299.github.io/TextUtils-React/static/js/main.5a4ab6f8.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "origin%20https://Harshit2299.github.io/TextUtils-React/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "origin%20https://Harshit2299.github.io/TextUtils-React/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "62b6420eca7698cce5b6",
+    "url": "origin%20https://Harshit2299.github.io/TextUtils-React/static/js/runtime~main.62b6420e.js"
+  },
+  {
+    "revision": "d926030054ea174f5afe0a3c1bd57e8c",
+    "url": "origin%20https://Harshit2299.github.io/TextUtils-React/index.html"
+  }
+];
